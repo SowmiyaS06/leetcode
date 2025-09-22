2f95b99cbdb1ecc7f72b84aa1f36a5ae91acf7d8
SowmiyaS06 <sowmiyaselvaraj9025@gmail.com>
2025-09-22 11:19:55 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 73.11%] - LeetPush

--- Climbing-Stairs.cpp
+++ Climbing-Stairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int solve(int n){
-        if(n<=2)return n;
+    int climbStairs(int n) {
+       if(n<=2)return n;
         int a=1,b=2,c=0;
         for(int i=3;i<=n;i++){
             c=a+b;
@@ -9,8 +9,5 @@ public:
             b=c;
         }
         return c;
-    }
-    int climbStairs(int n) {
-        return solve(n);
     }
 };
