5afe2346a5af53627cc5aefc354864faae84de6c
SowmiyaS06 <sowmiyaselvaraj9025@gmail.com>
2025-09-22 10:18:25 +0530

    [509] [Time Beats: 100.00%] [Memory Beats: 11.06%] - LeetPush

--- Fibonacci-Number.cpp
+++ Fibonacci-Number.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
+int solve(int n,vector<int>& dp){
+    if(n==0||n==1)return n;
+    else if(dp[n]!=-1) return dp[n];
+    else return dp[n] = solve(n-1,dp) + solve(n-2,dp);
+}
     int fib(int n) {
-        if(n==0) return 0;
-        else if(n==1) return 1;
-        else return fib(n-1)+fib(n-2);
-        
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);    
     }
-};
+};//tc=o(n)sc=o(n)
