cb38b2edc95cb980ac69e9d9aa873a92e5874aa9
SowmiyaS06 <sowmiyaselvaraj9025@gmail.com>
2025-09-22 14:16:13 +0530

    [746] [Time Beats: 100.00%] [Memory Beats: 89.44%] - LeetPush

--- Min-Cost-Climbing-Stairs.cpp
+++ Min-Cost-Climbing-Stairs.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-int solve(int i,vector<int>&cost,vector<int>&dp){
-    if(i>=cost.size()) return 0;
-    if(dp[i]!=-1) return dp[i];
-    else return dp[i]=cost[i]+min(solve(i+1,cost,dp),solve(i+2,cost,dp));
-}
     int minCostClimbingStairs(vector<int>& cost) {
-        vector<int> dp(cost.size()+1,-1);
-        return min(solve(0,cost,dp),solve(1,cost,dp));
+        int n = cost.size();
+        int p = cost[0];
+        int q = cost[1];
+        int c;
+
+        for (int i = 2; i < n; i++) {
+           c=cost[i]+min(p,q);
+           p=q;
+           q=c;
+        }
+
+        return min(p,q);
     }
 };
