f0041bdc75b141e77563baf126bdcd7db38b8e5e
SowmiyaS06 <sowmiyaselvaraj9025@gmail.com>
2025-09-22 15:57:25 +0530

    [64] [Time Beats: 16.71%] [Memory Beats: 62.09%] - LeetPush

---
+++ Minimum-Path-Sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int solve(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+        int m = grid.size(), n = grid[0].size();
+        if (i >= m || j >= n) return INT_MAX;     
+        if (i == m - 1 && j == n - 1) return grid[i][j]; 
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int down = solve(i + 1, j, grid, dp);
+        int right = solve(i, j + 1, grid, dp);
+
+        return dp[i][j] = grid[i][j] + min(down, right);
+    }
+
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(0, 0, grid, dp);
+    }
+};
