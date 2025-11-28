eb146cb00cef8158950e4fe1993c0278faf8e523
SowmiyaS06 <sowmiyaselvaraj9025@gmail.com>
2025-11-28 14:10:16 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (93.76%) - LeetSync

---
+++ 61-rotate-list/rotate-list.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+         if(head==NULL||head->next==NULL||k==0) return head;
+        int n=1;
+        ListNode * tail=head;
+        while(tail->next){
+            tail=tail->next;
+            n++;
+        }
+        k%=n;
+        if(k==0) return head;
+        int steps = n-k;
+        int i=1;
+        ListNode *newTail=head,*newHead;
+        while(i<steps){
+            newTail=newTail->next;
+            i++;
+    }
+        tail->next=head;
+        newHead=newTail->next;
+        newTail->next=NULL;
+    return newHead;
+    }
+};
