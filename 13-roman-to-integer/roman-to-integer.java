737a48b45fb0d56dfe8820888e9ed03892306c2e
SowmiyaS06 <sowmiyaselvaraj9025@gmail.com>
2025-11-30 11:40:11 +0530

    Time: 4 ms (68.44%) | Memory: 46.4 MB (25.54%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,82 +1,25 @@
 class Solution {
-    public int romanToInt(String s) {
-        int n = s.length();
-        int tot = 0;
-
-        for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-
-            switch (ch) {
-
-                case 'I':
-                    if (i < n - 1) {
-                        char next = s.charAt(i + 1);
-                        if (next == 'V') {
-                            tot += 4;
-                            i++; 
-                            break;
-                        }
-                        if (next == 'X') {
-                            tot += 9;
-                            i++;
-                            break;
-                        }
-                    }
-                    tot += 1;
-                    break;
-
-                case 'V':
-                    tot += 5;
-                    break;
-
-                case 'X':
-                    if (i < n - 1) {
-                        char next = s.charAt(i + 1);
-                        if (next == 'L') {
-                            tot += 40;
-                            i++;
-                            break;
-                        }
-                        if (next == 'C') {
-                            tot += 90;
-                            i++;
-                            break;
-                        }
-                    }
-                    tot += 10;
-                    break;
-
-                case 'L':
-                    tot += 50;
-                    break;
-
-                case 'C':
-                    if (i < n - 1) {
-                        char next = s.charAt(i + 1);
-                        if (next == 'D') {
-                            tot += 400;
-                            i++;
-                            break;
-                        }
-                        if (next == 'M') {
-                            tot += 900;
-                            i++;
-                            break;
-                        }
-                    }
-                    tot += 100;
-                    break;
-
-                case 'D':
-                    tot += 500;
-                    break;
-
-                case 'M':
-                    tot += 1000;
-                    break;
-            }
+     private int value(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
         }
-
-        return tot;
+        return 0;
+    }
+    public int romanToInt(String s) {
+        int total=0;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            int curr=value(s.charAt(i));
+            if(i+1<n&&curr<value(s.charAt(i+1)))
+            total-=curr;
+            else total+=curr;
+        }
+        return total;
     }
 }
