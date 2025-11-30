7fbecd5d0b60f2fe78431f358db9b37363454d38
SowmiyaS06 <sowmiyaselvaraj9025@gmail.com>
2025-11-30 09:53:24 +0530

    Time: 6 ms (33.53%) | Memory: 12.8 MB (20.54%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        unordered_map<string, int> mp = {
+        vector<pair<string, int>> roman = {
             {"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400},
             {"C", 100}, {"XC", 90}, {"L", 50}, {"XL", 40},
             {"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}
         };
-        vector<string> order={
-            "M", "CM", "D", "CD",
-            "C", "XC", "L", "XL",
-            "X", "IX", "V", "IV", "I"
-        };
         string res="";
-        for(int i=0;i<order.size();i++){
-            string sym=order[i];
-            while(num>=mp[sym]){
-                res+=sym;
-                num-=mp[sym];
+        for(int i=0;i<roman.size();i++){
+            while(num>=roman[i].second){
+                res+=roman[i].first;
+                num-=roman[i].second;
             }
         }
         return res;
