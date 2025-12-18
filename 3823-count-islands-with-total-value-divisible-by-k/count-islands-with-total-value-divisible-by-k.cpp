cadbaa9d449beceac5efe3cd44a0bcee8026315f
SowmiyaS06 <sowmiyaselvaraj9025@gmail.com>
2025-12-18 14:20:33 +0530

    Time: 12 ms (99.23%) | Memory: 83.2 MB (83.75%) - LeetSync

---
+++ 3823-count-islands-with-total-value-divisible-by-k/count-islands-with-total-value-divisible-by-k.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+long long sum=0;
+void dfs(vector<vector<int>>&grid,int r,int c){
+    if(r<0||c<0||r>=grid.size()||c>=grid[0].size()||grid[r][c]==0) return;
+    sum+=grid[r][c];
+    grid[r][c]=0;
+    dfs(grid,r,c+1);//right
+    dfs(grid,r+1,c);//down
+    dfs(grid,r,c-1);//left
+    dfs(grid,r-1,c);//right
+}
+    int countIslands(vector<vector<int>>& grid, int k) {
+        int m=grid.size();
+        int n=grid[0].size();
+        int count=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]!=0){
+                    sum=0;
+                    dfs(grid,i,j);
+                    if(sum%k==0)count++;
+
+                }
+            }
+        }
+    return count;
+    }
+};
